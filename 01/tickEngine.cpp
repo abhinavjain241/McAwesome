767f3dcfb49a60a64a3ff42999478744eec065b3
Abhinav Jain <abhinavjain241@gmail.com>
2015-09-25 19:37:37 +0530

    Initial Commit

---
+++ 01/tickEngine.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	map <int, map<string, map<string, int> > > data;
+	// data[10]["s1"]["f1"] = 100;
+	unsigned int entries;
+	scanf("tickfile %d", &entries);
+	for (int i = 0; i < entries; ++i) {
+		
+	}
+	// cout << "Data is:" << data[10]["s1"]["f1"] << endl;  
+	return 0;
+}
